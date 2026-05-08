28babb964c2010353412d89fc00a0dad031d4036
xiangxiang818 <navipin.hongp@mail.kmutt.ac.th>
2026-05-08 21:20:25 +0700

    feat: add login page and related widgets

---
+++ lib/features/auth/presentation/widgets/login_submit_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../../../../core/theme/tokens/typography/app_font_sizes.dart';
+import '../../../../core/theme/tokens/typography/app_font_families.dart';
+import '../../../../core/theme/tokens/colors/app_color_palette.dart';
+
+class LoginButton extends StatelessWidget {
+  final String text;
+
+  const LoginButton({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: SizedBox(
+        width: double.infinity,
+        height: 40,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColorPalette.deepViolet,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: AppFontSizes.base,
+              fontFamily: AppFontFamilies.kanit,
+              fontWeight: FontWeight.w600,
+              color: AppColorPalette.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
